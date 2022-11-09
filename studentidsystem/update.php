70487500ef9d67ab1331dc43a205b0ef903e17ed
JPveloria <JPveloria@github.com>
2022-11-08 20:46:57 -0500

    final

--- studentidsystem/update.php
+++ studentidsystem/update.php
@@ -5,20 +5,22 @@ $msg = '';
 if (isset($_GET['id'])) {
     if (!empty($_POST)) {
         $id = isset($_POST['id']) ? $_POST['id'] : NULL;
-        $name = isset($_POST['name']) ? $_POST['name'] : '';
-        $email = isset($_POST['email']) ? $_POST['email'] : '';
-        $phone = isset($_POST['phone']) ? $_POST['phone'] : '';
-        $title = isset($_POST['title']) ? $_POST['title'] : '';
+        $fname = isset($_POST['fname']) ? $_POST['fname'] : '';
+        $lname = isset($_POST['lname']) ? $_POST['lname'] : '';
+        $mname = isset($_POST['mname']) ? $_POST['mname'] : '';
+        $birthdate = isset($_POST['birthdate']) ? $_POST['birthdate'] : '';
+        $pgname = isset($_POST['pgname']) ? $_POST['pgname'] : '';
+        $contact = isset($_POST['contact']) ? $_POST['contact'] : '';
         $created = isset($_POST['created']) ? $_POST['created'] : date('Y-m-d H:i:s');
-        $stmt = $pdo->prepare('UPDATE contacts SET id = ?, name = ?, email = ?, phone = ?, title = ?, created = ? WHERE id = ?');
+        $stmt = $pdo->prepare('UPDATE studentid SET id = ?, fname = ?, lname = ?, mname = ?, birthdate = ?, pgname = ?, contact = ?, created = ? WHERE id = ?');
         $stmt->execute([$id, $name, $email, $phone, $title, $created, $_GET['id']]);
         $msg = 'Updated Successfully!';
     }
-    $stmt = $pdo->prepare('SELECT * FROM contacts WHERE id = ?');
+    $stmt = $pdo->prepare('SELECT * FROM studentid WHERE id = ?');
     $stmt->execute([$_GET['id']]);
-    $contact = $stmt->fetch(PDO::FETCH_ASSOC);
-    if (!$contact) {
-        exit('Contact doesn\'t exist with that ID!');
+    $student = $stmt->fetch(PDO::FETCH_ASSOC);
+    if (!$student) {
+        exit('Student doesn\'t exist with that ID!');
     }
 } else {
     exit('No ID specified!');
@@ -28,21 +30,33 @@ if (isset($_GET['id'])) {
 <?=template_header('Read')?>
 
 <div class="content update">
-    <h2>Update Contact #<?=$contact['id']?></h2>
-    <form action="update.php?id=<?=$contact['id']?>" method="post">
+    <h2>Update Student #<?=$student['id']?></h2>
+    <form action="update.php?id=<?=$student['id']?>" method="post">
         <label for="id">ID</label>
-        <label for="name">Name</label>
-        <input type="text" name="id" placeholder="1" value="<?=$contact['id']?>" id="id">
-        <input type="text" name="name" placeholder="John Doe" value="<?=$contact['name']?>" id="name">
-        <label for="email">Email</label>
-        <label for="phone">Phone</label>
-        <input type="text" name="email" placeholder="johndoe@example.com" value="<?=$contact['email']?>" id="email">
-        <input type="text" name="phone" placeholder="2025550143" value="<?=$contact['phone']?>" id="phone">
-        <label for="title">Title</label>
+        <label for="fname">FirstName</label>
+        
+        <input type="text" name="id" placeholder="" value="<?=$student['id']?>" id="id">
+        <input type="text" name="name" placeholder="" value="<?=$student['fname']?>" id="fname">
+
+        <label for="lname">LastName</label>
+        <input type="text" name="lname" placeholder="" value="<?=$student['lname']?>" id="lname">
+
+        <label for="mname">MiddleName</label>
+        <input type="text" name="mname" placeholder="" value="<?=$student['mname']?>" id="mname">
+
+        <label for="birthdate">Birthdate</label>
+        <input type="text" name="birthdate" placeholder="" value="<?=$student['birthdate']?>" id="birthdate">
+
+        <label for="pgname">Parent/Guardian Fullname</label>
+        <input type="text" name="pgname" placeholder="" value="<?=$student['pgname']?>" id="pgname">
+
+        <label for="contact">Contact</label>
+        <input type="text" name="contact" placeholder="" value="<?=$student['contact']?>" id="contact">
+
         <label for="created">Created</label>
-        <input type="text" name="title" placeholder="Employee" value="<?=$contact['title']?>" id="title">
-        <input type="datetime-local" name="created" value="<?=date('Y-m-d\TH:i', strtotime($contact['created']))?>" id="created">
+        <input type="datetime-local" name="created" value="<?=date('Y-m-d\TH:i', strtotime($student['created']))?>" id="created">
         <input type="submit" value="Update">
+
     </form>
     <?php if ($msg): ?>
     <p><?=$msg?></p>
